e28626a8156282626ce44225e2292ebd75927920
FreddyA12 <2balvarezfreddy@gmail.com>
2024-05-10 00:53:27 -0500

    Se creo la clase personas, con sus atributos

---
+++ gestor-reservas-aulas/src/entidades/Persona.java
@@ -0,0 +1,49 @@
+package entidades;
+
+public class Persona {
+
+    private int id;
+    private int cedula;
+    private String nombre;
+    private String apellido;
+
+    public Persona(int id, int cedula, String nombre, String apellido) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+}
