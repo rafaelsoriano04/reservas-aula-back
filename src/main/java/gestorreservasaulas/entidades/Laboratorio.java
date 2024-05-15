0c237723c66ee84f4251043e82a1851ee7972355
alisonMSalas <asalas2854@uta.edu.ec>
2024-05-15 11:00:36 -0500

    Se agrego la entidad y servicios de laboratorio

---
+++ src/main/java/gestorreservasaulas/entidades/Laboratorio.java
@@ -0,0 +1,34 @@
+package gestorreservasaulas.entidades;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "laboratorios")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Laboratorio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
+    private String nombre;
+    private int piso;
+    private int capacidad;
+
+    @OneToMany(mappedBy = "laboratorios", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Reserva> listaReserva;
+
+    @OneToMany(mappedBy = "laboratorios", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Horario> listaHorario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_bloque")
+    private Bloque bloque;
+
+}
