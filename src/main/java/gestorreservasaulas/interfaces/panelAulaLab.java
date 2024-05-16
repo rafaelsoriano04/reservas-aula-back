9b9ea0d3c56c38340eb3aab830a84e23e5dd98cb
melanieAlban <malban5548@uta.edu.ec>
2024-05-16 12:12:17 -0500

    se creo la estructura de la interfaz de aulas y laboratorios

--- src/main/java/gestorreservasaulas/interfaces/panelAulaLab.java
+++ src/main/java/gestorreservasaulas/interfaces/panelAulaLab.java
@@ -26,16 +26,8 @@ public class panelAulaLab extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(0, 300, Short.MAX_VALUE)
-        );
+        setBackground(new java.awt.Color(255, 255, 255));
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
     }// </editor-fold>//GEN-END:initComponents
 
 
