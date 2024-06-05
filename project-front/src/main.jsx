e5ccbefd1c42729b3f6ad9af6cd279aa2ded9680
nanobotrs <raansopro324@gmail.com>
2024-06-05 11:04:36 -0500

    Se agrego las dependencias de eslint y solucionar dependencias innecesarias

--- project-front/src/main.jsx
+++ project-front/src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import 'jquery';
-import 'popper.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
