e46afc04c6603183d1de0258b3fbac74d28045d7
FreddyA12 <2balvarezfreddy@gmail.com>
2024-07-05 13:28:46 -0500

    Se arreglo el metodo para crear y editar docentes

--- project-front/src/components/docentes.jsx
+++ project-front/src/components/docentes.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Form, Button, Modal } from "react-bootstrap";
+import { Form, Button, Modal, FormControl } from "react-bootstrap";
 import "../styles/docentes.css";
 import axios from "axios";
 import ReactPaginate from "react-paginate";
 import { FaPlus } from "react-icons/fa";
-import { ok, oops, deleteConfirmation, info } from "../utils/Alerts";
+import { ok, oops, deleteConfirmation, info, warning } from "../utils/Alerts";
+import { Input } from "react-select/animated";
 
 function Docentes() {
   // Variables
@@ -49,7 +50,76 @@ function Docentes() {
     }
   }, [filtroNombre, filtroCedula]);
 
+
+    
   // Funciones
+  function validarcedula(cedula){
+    
+    if(cedula.length == 10){
+        
+      //Obtenemos el digito de la region que sonlos dos primeros digitos
+      var digito_region = cedula.substring(0,2);
+      
+      //Pregunto si la region existe ecuador se divide en 24 regiones
+      if( digito_region >= 1 && digito_region <=24 ){
+        
+        // Extraigo el ultimo digito
+        var ultimo_digito   = cedula.substring(9,10);
+
+        //Agrupo todos los pares y los sumo
+        var pares = parseInt(cedula.substring(1,2)) + parseInt(cedula.substring(3,4)) + parseInt(cedula.substring(5,6)) + parseInt(cedula.substring(7,8));
+
+        //Agrupo los impares, los multiplico por un factor de 2, si la resultante es > que 9 le restamos el 9 a la resultante
+        var numero1 = cedula.substring(0,1);
+        var numero1 = (numero1 * 2);
+        if( numero1 > 9 ){ var numero1 = (numero1 - 9); }
+
+        var numero3 = cedula.substring(2,3);
+        var numero3 = (numero3 * 2);
+        if( numero3 > 9 ){ var numero3 = (numero3 - 9); }
+
+        var numero5 = cedula.substring(4,5);
+        var numero5 = (numero5 * 2);
+        if( numero5 > 9 ){ var numero5 = (numero5 - 9); }
+
+        var numero7 = cedula.substring(6,7);
+        var numero7 = (numero7 * 2);
+        if( numero7 > 9 ){ var numero7 = (numero7 - 9); }
+
+        var numero9 = cedula.substring(8,9);
+        var numero9 = (numero9 * 2);
+        if( numero9 > 9 ){ var numero9 = (numero9 - 9); }
+
+        var impares = numero1 + numero3 + numero5 + numero7 + numero9;
+
+        //Suma total
+        var suma_total = (pares + impares);
+
+        //extraemos el primero digito
+        var primer_digito_suma = String(suma_total).substring(0,1);
+
+        //Obtenemos la decena inmediata
+        var decena = (parseInt(primer_digito_suma) + 1)  * 10;
+
+        //Obtenemos la resta de la decena inmediata - la suma_total esto nos da el digito validador
+        var digito_validador = decena - suma_total;
+
+        //Si el digito validador es = a 10 toma el valor de 0
+        if(digito_validador == 10)
+          var digito_validador = 0;
+
+        //Validamos que el digito validador sea igual al de la cedula
+        if(digito_validador == ultimo_digito){
+          return true;
+        }else{
+          return false;
+        }
+        
+      }
+   }else{
+      return false;
+   }    
+  }
   const crearDocente = async () => {
     try {
       let docente = {
@@ -60,6 +130,12 @@ function Docentes() {
         direccion: "",
         tipo: "Docente",
       };
+      
+      if (!validarcedula(docente.cedula)) {
+        oops("Cédula no Valida");
+        return;
+      }
+      
       await axios.post(`http://localhost:8080/person`, docente);
       getDocentes();
       setFormData({
@@ -69,11 +145,21 @@ function Docentes() {
         apellido: "",
         telefono: "",
       });
+      handleCloseModal();
       ok("Registro guardado exitosamente.");
     } catch (error) {
-      oops("No se pudo guardar el registro. Por favor, inténtelo de nuevo.");
+      if (error.response) {
+        const { message } = error.response.data;
+        if (message == "cedula existe") {
+          warning("La cédula ya existe");
+        }
+      } else {
+        oops("No se pudo guardar el registro. Por favor, inténtelo de nuevo.");
+      }
+
     }
   };
+
 
   const getDocentes = async () => {
     let url;
@@ -120,11 +206,12 @@ function Docentes() {
     }
   };
 
+
   const editarDocente = async () => {
     const url = `http://localhost:8080/person`;
     let docente = {
       id: formData.id,
-      cedula: formData.cedula,
+      cedula: "editando",
       nombre: formData.nombre,
       apellido: formData.apellido,
       telefono: formData.telefono,
@@ -144,9 +231,19 @@ function Docentes() {
           telefono: "",
         });
         ok("Registro actualizado exitosamente.");
+        handleCloseModal();
       }
     } catch (error) {
-      oops("No se pudo actualizar el registro. Por favor, inténtelo de nuevo.");
+
+      if (error.response) {
+        const { message } = error.response.data;
+        if (message == "cedula existe") {
+          warning("La cédula ya existe");
+        }
+      } else {
+        oops("No se pudo actualizar el registro. Por favor, inténtelo de nuevo.");
+      }
+
     }
   };
 
@@ -353,17 +450,43 @@ function Docentes() {
               <div className="col-12">
                 <Form.Group className="form-group">
                   <Form.Label htmlFor="cedula">Cédula:</Form.Label>
-                  <Form.Control
-                    type="number"
+                  {!isEditing ? (<Form.Control
+                    type="text"
                     id="cedula"
                     className="form-control"
                     name="cedula"
                     value={formData.cedula}
+                    maxLength={10}
                     placeholder="Ingrese su cédula"
-                    onChange={e =>
-                      setFormData({ ...formData, cedula: e.target.value })
-                    }
-                  />
+                    onChange={(e) => {
+                      // Acepta solo valores numéricos
+                      const re = /^[0-9\b]+$/;
+                      if (e.target.value === '' || re.test(e.target.value)) {
+                        setFormData({ ...formData, cedula: e.target.value });
+                      }
+                    }}
+                    
+                  />) : (<Form.Control
+                    type="text"
+                    id="cedula"
+                    className="form-control"
+                    name="cedula"
+                    value={formData.cedula}
+                    maxLength={10}
+                    placeholder="Ingrese su cédula"
+                    onChange={(e) => {
+                      // Acepta solo valores numéricos
+                      const re = /^[0-9\b]+$/;
+                      if (e.target.value === '' || re.test(e.target.value)) {
+                        setFormData({ ...formData, cedula: e.target.value });
+                      }
+                    }}
+                    readOnly
+                    
+                  />)}
+                  
+                  
+
                 </Form.Group>
               </div>
               <div className="col-12">
@@ -406,7 +529,7 @@ function Docentes() {
                   className="btn btn-custom"
                   onClick={() => {
                     crearDocente();
-                    handleCloseModal();
+
                   }}
                 >
                   Crear
