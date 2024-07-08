bdf4aac341741bab7cdc32969a29a75ef1df15ff
nanobotrs <raansopro324@gmail.com>
2024-07-07 23:47:18 -0500

    Configuración producción front

--- project-front/vite.config.js
+++ project-front/vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Esto permite que Vite acepte conexiones externas
+    port: 5173,
+  },
 });
