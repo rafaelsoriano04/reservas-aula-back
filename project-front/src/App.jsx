62ca81465ab692915c73bae3d5d9a194e01b0b4e
nanobotrs <raansopro324@gmail.com>
2024-07-11 13:33:55 -0500

    Formato

--- project-front/src/App.jsx
+++ project-front/src/App.jsx
@@ -1,35 +1,36 @@
-import React from "react";
-import { PrimeReactProvider } from "primereact/api";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "primereact/resources/themes/lara-light-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import React from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+
 import Menu from "./components/Menu";
 import Login from "./components/Login";
 import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
   return (
-    <PrimeReactProvider>
-      <Router>
-        <Routes>
-          <Route>
-            <Route path="/" element={<Navigate to="/login" />} />
-          </Route>
-          <Route>
-            <Route path="/login" element={<Login />} />
-          </Route>
-          <Route element={<PrivateRoute />}>
-            <Route path="/menu" element={<Menu />} />
-          </Route>
-        </Routes>
-      </Router>
-    </PrimeReactProvider>
+    <Router>
+      <Routes>
+        <Route>
+          <Route path="/" element={<Navigate to="/login" />} />
+        </Route>
+        <Route>
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/menu" element={<Menu />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
